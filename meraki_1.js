ab4996dcf424a1b6f12e41277b26cf8bb93423e3
Pujarani06052001 <103930803+Pujarani06052001@users.noreply.github.com>
2022-10-28 20:16:04 +0530

    Add files via upload

---
+++ meraki_1.js
@@ -0,0 +1,33 @@
+// console.log(36/4)
+
+// console.log(10 - 4 * 2)
+
+
+
+// a = [10, 20]
+// b = a
+// b += [30, 40]
+// console.log(a)
+// console.log(b)
+
+
+// x = 100
+// y = 50
+// console.log(x && y)
+
+// x = 6
+// y = 2
+// console.log(x ** y)
+
+
+
+
+// y = 10
+// x = y += 2
+// console.log(x)
+
+
+
+// const x = 5, y = 3;
+// (x < 6) && (y < 5); // true
+// console.log(true && false); // false
---
+++ op_q.js
@@ -0,0 +1,6 @@
+console.log(!0);
+console.log(!"");
+console.log(!false);
+console.log(!NaN);
+console.log(!undefined);
+console.log(!null);
